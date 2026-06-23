1e663b19b6d57b308fd496e75bbf55607d2261d9
mrahenderson26 <mrahenderson26@gmail.com>
2026-06-23 14:27:50 +0200

    class

---
+++ Intranets-Extranets/23junio/ejercicios03/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import MostrarUsuarios from './components/MostrarUsuarios'
+
+function App() {
+
+  return (
+    <>
+    <div className="container">
+      <h1>Api Fetch en React</h1>
+      <MostrarUsuarios />
+    </div>
+    </>
+  )
+}
+
+export default App
+ 
