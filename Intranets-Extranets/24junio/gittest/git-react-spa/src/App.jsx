5c9ca203eb6c89f4269a89be5aeeeb9aad2e6044
Panopticon Planet <panopticonplanet@gmail.com>
2026-06-24 18:50:06 +0200

    Initial React SPA baseline

---
+++ Intranets-Extranets/24junio/gittest/git-react-spa/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <main style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>Git Test SPA</h1>
+      <p>This is the original version on main.</p>
+      <p>Next step: create a dev branch and change this page.</p>
+    </main>
+  )
+}
+
+export default App
